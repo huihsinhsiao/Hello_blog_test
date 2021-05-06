e1f166debc29264d67cfea612d9a60d65367f5d9
Noel Hsiao <huihsinhsiao12@gmail.com>
2021-05-06 16:10:52 +0800

    新增paranoia套件及相關欄位

---
+++ db/migrate/20210506070052_add_deleted_at_to_clients.rb
@@ -0,0 +1,7 @@
+class AddDeletedAtToClients < ActiveRecord::Migration[6.1]
+  def change
+    add_column :posts, :deleted_at, :datetime
+    add_index :posts, :deleted_at
+    
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_25_114131) do
+ActiveRecord::Schema.define(version: 2021_05_06_075909) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2021_04_25_114131) do
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "deleted_at"
+    t.index ["deleted_at"], name: "index_posts_on_deleted_at"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
@@ -28,6 +30,8 @@ ActiveRecord::Schema.define(version: 2021_04_25_114131) do
     t.string "tel"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "deleted_at"
+    t.index ["deleted_at"], name: "index_users_on_deleted_at"
   end
 
   add_foreign_key "posts", "users"
